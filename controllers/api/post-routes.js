b26740c82ac351cb0cf05ccf14ee6ecd6df68d0a
Tooboi <japica123@gmail.com>
2023-01-02 01:12:20 -0500

    added edit capability

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -79,4 +79,48 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
+router.put('/:id', withAuth, (req, res) => {
+  Post.update(
+    {
+      post_header: req.body.post_header,
+      post_body: req.body.post_body,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
