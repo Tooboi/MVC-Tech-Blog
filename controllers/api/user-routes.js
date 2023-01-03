66f7e549082b22d2e2529afabebb303fa86ee01b
Tooboi <japica123@gmail.com>
2023-01-02 21:02:14 -0500

    styling and cleaning, comments display

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
   // Access our User model and run .findAll() method
